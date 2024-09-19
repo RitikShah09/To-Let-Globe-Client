c75f76dda9b7094a7589e8ee1dd3e50ac263d355
Ritik <109175397+RitikShah09@users.noreply.github.com>
2024-09-19 23:21:20 +0530

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -5,14 +5,4 @@ const instance = axios.create({
   baseURL: "https://to-let-globe-api.onrender.com/",
   withCredentials: true,
 });
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 export default instance;
